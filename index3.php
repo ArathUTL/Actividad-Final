310aeabc78f858f4932f45a878c45036b0751dc4
arath <brianarathps@gmail.com>
2021-11-27 02:54:42 -0600

    commit 1

---
+++ index3.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="index.php">Logo de la empresa</a>
+    <button class="navbar-toggler" type="button" data-target="#my-nav" data-toggle="collapse">
+          <span class="navbar-toggler-icon"></span>
+    </button>      
+    <div id="my-nav" class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Homeaaaa</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Carrito(0)</a>
+        </li>
+      </ul>
+  </nav>
+  <br/>
+  <br/>
+  <div class="container">
+   <br>
+   <div class="alert alert-success">
+       Pantalla de mensaje...
+       <a href="#" class="badge badge-success">ver carrito<a/>
+   </div>
+   <div class="row">
+     <div class="col-3">
+     <div class="card">
+           <img class="card-img-top" src=""
+           title="titulo prodicto"
+           alt="titulo"
+           class="card-img-top"
+           img src="imagenes/portfolio-04.PNG">
+         <div class="card-body">
+         <span>Nike Jordan</span>
+             <h5 class="card-title">$300.00</h5>
+             <p class="Media text">25mx-30mx</p>
+             <button class="btn btn-primary" 
+             name="btnAccion" 
+             value="Agregar" 
+             type="submit">
+             Agregar al carrito
+             </button>
+         </div>
+       </div>
+     </div>
+  </div>    
+</body>
+</html>
